017e03866fbefe9c7e6667035d2ca70d93ffa96e
NL647 <76846739+NL647@users.noreply.github.com>
2021-01-03 15:16:39 +0100

    Add files via upload

---
+++ erease_spiffs.ino
@@ -0,0 +1,39 @@
+#include "SPIFFS.h"
+ 
+void listAllFiles(){
+ 
+  File root = SPIFFS.open("/");
+ 
+  File file = root.openNextFile();
+ 
+  while(file){
+ 
+      Serial.print("FILE: ");
+      Serial.println(file.name());
+ 
+      file = root.openNextFile();
+  }
+ 
+}
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+ 
+  Serial.println("\n\n----Listing files before format----");
+  listAllFiles();
+ 
+  //bool formatted = SPIFFS.format();
+ 
+
+ 
+  Serial.println("\n\n----Listing files after format----");
+  listAllFiles();
+}
+ 
+void loop() {}
